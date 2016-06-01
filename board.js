a7292c354caebd1d5971f3c44b02d1357cf0b33b
lugDroid <lugdroid@gmail.com>
2016-06-01 19:32:40 +0100

    minimax algorith started

--- board.js
+++ board.js
@@ -29,6 +29,56 @@ function Board() {
     return board;
   }
 
+  // check if board is a winning board. If true returns winning player, else -1
+  this.isWin = function() {
+    for (var i = 0; i < SIZE; i++) {
+      // check horizontals
+      if (state[i][0] === state[i][1] && state[i][0] === state[i][2] && state[i][0] !== '-') {
+        return state[i][0];
+      }
+      // check verticals
+      if (state[0][i] === state[1][i] && state[0][i] === state[2][i] && state[0][i] !== '-') {
+        return state [0][i];
+      }
+    }
+    // check diagonals
+    if (state[0][0] === state[1][1] && state[0][0] === state[2][2] && state[0][0] !== '-') {
+      return state[0][0];
+    }
+    if (state[0][2] === state[1][1] && state[0][2] == state[2][0] && state[0][2] !== '-') {
+      return state[0][2];
+    }
+    // if no winning condition
+    return -1
+  }
+
+  // check if board is a draw
+  this.isDraw = function() {
+    // if board is win return inmediately
+    if (this.isWin() !== -1) {
+      return false;
+    } else {
+      var isDraw = true;
+      for (var i = 0; i < SIZE; i++) {
+        for (var j = 0; j < SIZE; j++) {
+          if (state[i][j] === '-') {
+            isDraw = false;
+          }
+        }
+      }
+      return isDraw;
+    }
+  }
+
+  // check is board is terminal, is either win or draw
+  this.isTerminal = function() {
+    if (this.isWin() !== -1 || this.isDraw()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   // prints board into console
   this.print = function() {
     var str = '';
@@ -42,28 +92,106 @@ function Board() {
   }
 }
 
-// generic function to generate all possible next moves for a given board and player (X or O)
+// GAME function to generate all possible next moves for a given board and player (X or O)
 function generate(board, player) {
   var result = []
   for (var i = 0; i < SIZE; i++) {
     for (var j = 0; j < SIZE; j++) {
         if (board.read(i, j) === '-') {
+          //console.log('Empty position found at ' + i + ', ' + j);
           newBoard = board.clone();
           newBoard.set(player, i, j);
           result.push(newBoard);
-          break;
+          //console.log(result.length + ' new boards generated so far');
         }
     }
   }
   return result;
 }
 
+// GAME - return game score
+function score(board, maxPlayer, minPlayer) {
+  if (board.isWin() === maxPlayer) {
+    console.log('Win: ' + maxPlayer);
+    return 10;
+  } else if (board.isWin() === minPlayer) {
+    console.log('Win: ' + minPlayer);
+    return -10;
+  } else {
+    console.log('Draw');
+    return 0;
+  }
+}
+var bestChoice;
+
+// GAME - minimax algorithm
+function minimax(board, maxPlayer) {
+  // check if board is terminal node
+  if (board.isTerminal()) {
+    board.print();
+    //console.log(score(board, 'X', 'O') + '\n');
+    return score(board, 'X', 'O');
+  }
+
+  var scores = [];
+  var nextMoves = [];
+
+
+  // if next turn is for minPlayer
+  if (maxPlayer) {
+    // generate all possible next moves
+    nextMoves = generate(board, 'X');
+    // call minimax in all generated boards
+    for (var i = 0; i < nextMoves.length; i++) {
+      scores.push(minimax(nextMoves[i], false));
+      var minScore = Math.min.apply(null, scores); // TODO: find min value in scores
+      bestChoice = nextMoves[scores.indexOf(minScore)];
+      return minScore;
+    }
+  } else {
+    // generate all possible next moves
+    nextMoves = generate(board, 'O');
+    // call minimax in all generated boards
+    for (var j = 0; j < nextMoves.length; j++) {
+      scores.push(minimax(nextMoves[j], true));
+      var maxScore = Math.max.apply(null, scores); // TODO: find max value in scores
+      bestChoice = nextMoves[scores.indexOf(minScore)];
+      return maxScore;
+    }
+  }
+}
+
 // tests
 var board = new Board();
 
-var boards = generate(board, 'X');
-console.log(boards.length);
-for (var i = 0; i < boards.length; i++) {
-  boards[i].print();
-  console.log('\n');
-}
+// example terminal draw board
+// board.set('O', 0, 0);
+// board.set('O', 0, 1);
+// board.set('X', 0, 2);
+// board.set('X', 1, 0);
+// board.set('X', 1, 1);
+// board.set('O', 1, 2);
+// board.set('O', 2, 0);
+// board.set('X', 2, 1);
+// board.set('X', 2, 2);
+
+// example non-terminal board
+board.set('O', 0, 0);
+board.set('O', 0, 1);
+board.set('X', 0, 2);
+board.set('-', 1, 0);
+board.set('X', 1, 1);
+board.set('O', 1, 2);
+board.set('O', 2, 0);
+board.set('X', 2, 1);
+board.set('X', 2, 2);
+
+board.print();
+console.log('\n');
+console.log(minimax(board, true));
+
+// var boards = generate(board, 'X');
+// for (var i = 0; i < boards.length; i++) {
+//   boards[i].print();
+//   console.log('\n');
+// }
