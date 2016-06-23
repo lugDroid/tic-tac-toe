9be380860c33774699735377ff6f072f3e9904d1
lugDroid <lugdroid@gmail.com>
2016-06-23 21:58:55 +0100

    user controls added

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@ $(document).ready(function() {
   // create new board
   // TODO: create a start button, bind it and move this part to
   // game.start() method
-  var board = game.start();
+  var board, nextMove;
 
   // cache DOM
   var $square = $('.row .square');
   var $squares = [[$('#00'), $('#01'), $('#02')],
                   [$('#10'), $('#11'), $('#12')],
                   [$('#20'), $('#21'), $('#22')]];
+  var $result = $('#result');
+  var $computerBtn = $('#computer');
+  var $playerBtn = $('#player');
 
   // bind events
   $square.click(playerMove);
+  $computerBtn.click(computerStart);
+  $playerBtn.click(playerStart);
 
   // render
   function render(board) {
@@ -21,35 +26,51 @@ $(document).ready(function() {
           $squares[i][j].addClass('fa fa-times');
         } else if (board.read(i, j) === 'O') {
           $squares[i][j].addClass('fa fa-circle-o');
+        } else {
+          $squares[i][j].removeClass('fa fa-circle-o fa-times');
         }
       }
     }
   }
 
-  // functions
+  // binded functions
   function playerMove(e) {
     var id = e.target.id;
-    console.log('Square clicked:' + id)
-    // update view and set square on board object
-    $('#' + id).addClass("fa fa-circle-o");
-    board.set('O', id[0], id[1]);
-    // calculate next move
-    var nextMove = game.nextMove(board);
+    var col = id[0];
+    var row = id[1];
 
-    board = nextMove.board;
-    score = nextMove.score;
+    // only if square is still empty
+    if (board.read(col, row) === '-') {
+      // update view and set square on board object
+      $('#' + id).addClass("fa fa-circle-o");
+      board.set('O', col, row);
 
-    render(board);
+      // calculate next move
+      nextMove = game.nextMove(board);
+      board = nextMove.board;
+      // render next board
+      render(board);
 
-    if (score > 0) {
-      $('h5').html('You lose');
-    } else if (score < 0) {
-      $('h5').html('You win');
+      // generate resul
+      if (nextMove.score >= 9) {
+        $('#result').html('You lost!');
+      } else if (nextMove.score <= -9) {
+        $('#result').html('You won!');
+      }
     }
+  }
 
+  function computerStart() {
+    board = game.start();
+    render(board);
+    nextMove = game.nextMove(board);
+    board = nextMove.board;
+    console.log(board);
+    render(board);
+  }
+
+  function playerStart() {
+    board = game.start();
+    render(board);
   }
 });
-
-// fontawesome classes to add
-// fa fa-times X
-// fa fa-circle-o O
